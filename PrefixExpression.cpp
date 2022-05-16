8fa6db510def4e5c1f41dd16846e8cb585ca9201
Ben Cyna <benjamincyna@gmail.com>
2022-05-16 18:11:54 +0930

    created prefixExpression class

---
+++ PrefixExpression.cpp
@@ -0,0 +1,11 @@
+#include "PrefixExpression.h"
+
+PrefixExpression::PrefixExpression()
+{
+
+}
+
+PrefixExpression::~PrefixExpression()
+{
+
+}
---
+++ PrefixExpression.h
@@ -0,0 +1,16 @@
+#ifndef PREFIXEXPRESSION_H
+#define PREFIXEXPRESSION_H
+
+#pragma once
+
+class PrefixExpression
+{
+public:
+    PrefixExpression();
+    ~PrefixExpression();
+
+private:
+
+};
+
+#endif
