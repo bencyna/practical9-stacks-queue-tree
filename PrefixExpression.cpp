ff462cf781c56b8c90bc21da281c8199f17663be
Ben Cyna <benjamincyna@gmail.com>
2022-05-20 14:55:55 +0930

    commit from the airplane

--- PrefixExpression.cpp
+++ PrefixExpression.cpp
@@ -6,6 +6,9 @@ using namespace std;
 PrefixExpression::PrefixExpression(string operation)
 {
     // loop through operation to create the binary tree structure add to stack, if number remove and make node
+    for (int i = 0; i < operation.length(); i++) {
+        
+    }
 }
 
 PrefixExpression::~PrefixExpression()
