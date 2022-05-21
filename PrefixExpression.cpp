d5c5df59d2c3d660c8864e3a82ad607c70d081de
Ben Cyna <benjamincyna@gmail.com>
2022-05-21 17:24:13 +0930

    added comment

--- PrefixExpression.cpp
+++ PrefixExpression.cpp
@@ -7,7 +7,13 @@ PrefixExpression::PrefixExpression(string operation)
 {
     // loop through operation to create the binary tree structure add to stack, if number remove and make node
     for (int i = 0; i < operation.length(); i++) {
-        
+        if (operation[i] == '+' || operation[i] == '-' || operation[i] == '*' || operation[i] == '/') {
+            // add to stack
+        }
+        else {
+            // pop top two items from the stack and build mini tree
+            // add comment
+        }
     }
 }
 
